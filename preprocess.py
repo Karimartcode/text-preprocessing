c60ca5aed8f63e486900224960f0d20154d1f3c5
Abdelkarim Mouachiq <abmou87@ulaval.ca>
2024-03-08 18:44:59 +0000

    add tokenizers

--- preprocess.py
+++ preprocess.py
@@ -4,3 +4,11 @@ import re
 def load_text(filepath):
     with open(filepath, 'r', encoding='utf-8') as f:
         return f.read()
+
+
+def tokenize_words(text):
+    return text.split()
+
+
+def tokenize_sentences(text):
+    return re.split(r'(?<=[.!?])\s+', text)
